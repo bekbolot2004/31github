f428c727e193751d5241b3d5a2854b64f39cc49e
Бекболот Жантаев <bekbolotzantaev@Bekbolots-MacBook-Pro.local>
2023-12-04 23:16:29 +0100

    Working in Footer

--- src/shared/socialMedia/ui/index.tsx
+++ src/shared/socialMedia/ui/index.tsx
@@ -3,17 +3,48 @@ import facebook from '../../assets/icons/facebook.svg'
 import telegram from '../../assets/icons/telegram.svg'
 import instagram from '../../assets/icons/instagram.svg'
 import vk from '../../assets/icons/vk.svg'
+import Link from 'next/link'
 
 export const SocialMedia = () => {
 
+  const socMediaList = [
+    {
+      id: 1,
+      name: "Facebook",
+      link: "",
+      icon: facebook
+    },
+    {
+      id: 2,
+      name: "Telegram",
+      link: "",
+      icon: telegram
+    },
+    {
+      id: 3,
+      name: "Instagram",
+      link: "",
+      icon: instagram
+    },
+    {
+      id: 4,
+      name: "Vk",
+      link: "",
+      icon: vk
+    }
+  ]
+
   return (
-    <div>
-      <Image
-        src={logo}
-        width={245}
-        height={65}
-        alt="Logo"
-      />
+    <div className='flex gap-1.5'>
+      {socMediaList.map((item) =>
+        <Link href={item.link} className='h-[50px] w-[50px] bg-white flex justify-center items-center rounded-full'>
+          <Image
+            src={item.icon}
+            width={30}
+            height={30}
+            alt="Icon"
+          />
+        </Link>)}
     </div>
   );
 };
--- src/widgets/Layout/Footer/ui/index.tsx
+++ src/widgets/Layout/Footer/ui/index.tsx
@@ -1,19 +1,85 @@
 import s from "./styles.module.scss";
 import logo from '../../../../shared/assets/logo.png'
 import Image from 'next/image'
+import { SocialMedia } from "@/shared/socialMedia";
 
 export const Footer = () => {
 
+  const footerLinks = [
+    {
+      id: 1,
+      name: "Сообщество",
+      links: [
+        {
+          id: 1,
+          name: "Реферальная программа",
+          link: ""
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: "Компания",
+      links: [
+        {
+          id: 1,
+          name: "О сервисе",
+          link: ""
+        }
+      ]
+    },
+    {
+      id: 3,
+      name: "Помощь",
+      links: [
+        {
+          id: 1,
+          name: "Свяжитесь с нами",
+          link: ""
+        }
+      ]
+    },
+    {
+      id: 4,
+      name: "Аренда",
+    },
+  ]
+
   return (
     <footer className=" bg-[#FBFAFA]">
-      <div className="container mx-auto p-4">
+      <div className="container mx-auto p-4 py-14">
+        <div className="flex justify-between border-b border-[#C4C4C4] pb-14">
+          <div className="flex">
+            <div className="mr-24">
+              <Image
+                src={logo}
+                width={205}
+                height={45}
+                alt="Logo"
+              />
+            </div>
+            <div>
+              <SocialMedia />
+            </div>
+          </div>
+          <div className="flex gap-20">
+            {footerLinks.map((item) =>
+              <div>
+                <h2 className=" text-xl font-semibold">{item.name}</h2>
+                {item.links?.map((item) => <div>{item.name}</div>)}
+              </div>
+            )}
+          </div>
+        </div>
         <div>
-          <Image
-            src={logo}
-            width={245}
-            height={65}
-            alt="Logo"
-          />
+          <div className="flex gap-[120px] mt-7 text-[#717171]">
+            <p>© 2021 Getdesk.</p>
+            <ul className="flex gap-16">
+              <li>Конфиденциальность</li>
+              <li>Условия использования</li>
+              <li>Реквизиты компании</li>
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
