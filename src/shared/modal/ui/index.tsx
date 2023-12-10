3e56a39edc225b8b3321b7c76398ea06774d5250
Bekbolot Zhantaev <45750628+bekbolot2004@users.noreply.github.com>
2023-12-10 01:56:13 +0100

    done modal

---
+++ src/shared/modal/ui/index.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import Image from 'next/image'
+import { useState } from 'react';
+
+export const Modal = () => {
+
+  const [model, setModel] = useState(false)
+
+  return (
+    <div>
+
+    </div>
+  );
+};
